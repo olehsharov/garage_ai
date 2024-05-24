f031265bb1f334f6caccdffa20bfc57d7d993d03
osharovtw <o.sharov@twistrobotics.com>
2024-05-24 23:22:09 +0300

    test base

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import garage_ai.vector_db.db as db
 
-st.title('Hello World!')
-st.write('This is a Streamlit app!')
-st.write(db)
+st.set_page_config(page_title="Garage.ai")
+
+if query := st.chat_input("Type a query..."):
+    st.write(query)
